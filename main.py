710f6586bda83cad4f3a7f44fa2371f236052e64
Aryan Golbaghi <aryangolbaghi@gmail.com>
2025-03-04 10:15:42 -0800

    fixed some code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import data_preprocessing.download_data as download_data
 
 def initlaise():
     download_data.download_database()
-    m = download_data.generate_metadata_for_emodb()
-    print(m)
-    s = sum(m.values())
-    print(s)
+    # m = download_data.generate_metadata_for_emodb()
+    # print(m)
+    # s = sum(m.values())
+    # print(s)
 
     download_data.generate_train_test_sample()
 
